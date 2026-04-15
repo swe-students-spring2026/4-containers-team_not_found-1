56a6c6a18b6e2bc0fb2c99f75633f060db56e1cf
yashpaz123 <yrp6541@nyu.edu>
2026-04-15 13:20:11 -0400

    impl upload

--- web-app/main.py
+++ web-app/main.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/", methods=["GET", "POST"])
-def new_run():
+def index():
     if request.method == "GET":
         return render_template("index.html")
-    # if request.method == "POST":
-        # path 1: getting ai image recognition
+    if request.method == "POST":
+        image_bytes = request.get_data()
+        # TODO: do ai image recognition
+        image_description = "a cool doodle"
+        return image_description, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
