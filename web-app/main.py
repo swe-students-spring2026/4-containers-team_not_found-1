9d6d87048a93fb21ff450a93213c75057b92de97
Zhi-Hui-C <zc3716@nyu.edu>
2026-04-19 11:43:32 -0400

    chore: fix pylint missing docstring

--- web-app/main.py
+++ web-app/main.py
@@ -1,3 +1,7 @@
+"""
+Main web application for the ML Client.
+"""
+
 import os
 import random
 
@@ -53,7 +57,6 @@ def index():
         return render_template("index.html", thing=thing)
 
     if request.method == "POST":
-        # TODO: use this, store in db along with image bytes
         draw_instruction = request.headers.get('Draw-Instruction', 'unknown')
         image_bytes = request.get_data()
         if not image_bytes:
